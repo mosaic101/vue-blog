b20132d41c11a545f46fa2aff52af9b804102b06
mosaic101 <mosaic101@foxmail.com>
2017-07-19 23:40:15 +0800

    update

---
+++ design.md
@@ -0,0 +1,13 @@
+## Features
+
+1. topics
+2. home page
+3. message board
+4. open project
+5. search
+6. chatroom
+
+
+## HTML
+- home
+-
---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="test">
+  <h1>{{ message }}</h1>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="text">文字按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      message: 'test '
+    }
+  }
+}
+</script>
