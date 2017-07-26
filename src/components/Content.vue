3038a4f3d151deda4be47e493dab31dd4d268d29
mosaic101 <mosaic101@foxmail.com>
2017-07-26 23:45:54 +0800

    7-26

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,14 +1,55 @@
 <template>
   <div>
     <nav-menu></nav-menu>
-    <el-row>
-      <el-col :span="12">
-        <div class="grid-content bg-purple">2222</div>
-      </el-col>
-      <el-col :span="12">
-        <div class="grid-content bg-purple-light">11111</div>
-      </el-col>
-    </el-row>
+
+    <div class="container">
+      <el-row>
+        <el-col :xs="24" :md="18">
+
+          <article id="105" class="post">
+            <div class="post-head">
+              <h1 class="post-title">
+                <a href="/ghost-1-0-rc1/">Ghost 1.0 RC1 发布，可用于生产环境</a>
+              </h1>
+              <div class="post-meta">
+                <span class="author">作者：
+                  <a href="/author/wangsai/">王赛</a>
+                </span> •
+                <time class="post-date" datetime="2017年7月13日星期四中午12点12分" title="2017年7月13日星期四中午12点12分">2017年7月13日</time>
+              </div>
+            </div>
+            <div class="post-content">
+              <p>今天，我们正式发布了首个 Ghost 1.0 RC 版本，希望这是到达最终版本的最后一步。你可以通过 Ghost-CLI 安装此最新版本，并且可以使用到 生产环境中！！！如果你需要从 LTS (0.11) 版本迁移到 1.0 版本，请阅读迁移指南。 同时，下面列出从上一个 bet</p>
+            </div>
+            <div class="post-permalink">
+              <a href="/ghost-1-0-rc1/" class="btn btn-default">阅读全文</a>
+            </div>
+          </article>
+
+        </el-col>
+
+        <el-col :xs="24" :md="6">
+          <div class="post">
+            <div class="widget">
+              <h4 class="title">社区</h4>
+              <div class="content community">
+                <p>QQ群：277327792</p>
+                <p>
+                  <a href="http://wenda.ghostchina.com/" title="Ghost中文网问答社区" target="_blank" onclick="_hmt.push(['_trackEvent', 'big-button', 'click', '问答社区'])">
+                    <i class="fa fa-comments"></i> 问答社区</a>
+                </p>
+                <p>
+                  <a href="http://weibo.com/ghostchinacom" title="Ghost中文网官方微博" target="_blank" onclick="_hmt.push(['_trackEvent', 'big-button', 'click', '官方微博'])">
+                    <i class="fa fa-weibo"></i> 官方微博</a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </el-col>
+      </el-row>
+    </div>
+
   </div>
 </template>
 
@@ -25,41 +66,17 @@ export default {
 </script>
 
 <style lang="less">
-	@import '../style/mixin';
-	.data_section{
-		padding: 20px;
-		margin-bottom: 40px;
-		.section_title{
-			text-align: center;
-			font-size: 30px;
-			margin-bottom: 10px;
-		}
-		.data_list{
-			text-align: center;
-			font-size: 14px;
-			color: #666;
-            border-radius: 6px;
-            background: #E5E9F2;
-            .data_num{
-                color: #333;
-                font-size: 26px;
+.container {
+  max-width: 1360px;
+  margin: 30px auto; // padding: 0 15px;
+  border: 1px solid steelblue;
+}
 
-            }
-            .head{
-                border-radius: 6px;
-                font-size: 24px;
-                padding: 0 10px;
-                color: #fff;
-            }
-        }
-        .today_head{
-            background: #FF9800;
-        }
-        .all_head{
-            background: #20A0FF;
-        }
-	}
-    .wan{
-        .sc(16px, #333)
-    }
+.el-col {
+  padding: 15px; // border: 1px solid red;
+}
+
+.post {
+  border: 1px solid red;
+}
 </style>
